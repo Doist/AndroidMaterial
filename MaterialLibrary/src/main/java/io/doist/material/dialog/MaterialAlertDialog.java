d5f7c12bb456f5c4964e1f832b9219a1362e73b5
João Martins Costa <jpmcosta@gmail.com>
2015-03-21 01:34:34 +0000

    MaterialAlertDialog windowBackground supports Lollipop drawables

--- MaterialLibrary/src/main/java/io/doist/material/dialog/MaterialAlertDialog.java
+++ MaterialLibrary/src/main/java/io/doist/material/dialog/MaterialAlertDialog.java
@@ -2,6 +2,8 @@ package io.doist.material.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.view.Window;
 
 import io.doist.material.R;
 import io.doist.material.dialog.util.WindowCallbackWrapper;
+import io.doist.material.res.MaterialResources;
 
 /**
  * AlertDialog that enables the use of a custom layout as the dialog's
@@ -54,9 +57,13 @@ public class MaterialAlertDialog extends AlertDialog {
         public AlertDialog create() {
             AlertDialog dialog = super.create();
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                // On pre-L androids, intercept the content view creation
-                // and manipulate its view ids so that the framework can work with them.
-                interceptAndConfigureContentView(dialog);
+                // On pre-L androids, we will get and remove the window background drawable
+                // from the window context theme.
+                // We will also intercept the content view creation to manipulate its view ids
+                // so that the framework can work with them.
+                // When the content view is created we will also manually set the previously extracted
+                // windowBackground drawable.
+                configureWindowBackgroundAndContentView(dialog);
                 // Enforce consistent behavior of cancelling on outside touch.
                 // Some devices (Samsung, at least), removed this behavior from their dialogs.
                 if (mCancelable) {
@@ -66,13 +73,21 @@ public class MaterialAlertDialog extends AlertDialog {
             return dialog;
         }
 
-        private void interceptAndConfigureContentView(AlertDialog dialog) {
+        private void configureWindowBackgroundAndContentView(AlertDialog dialog) {
             final Window window = dialog.getWindow();
-            dialog.getWindow().setCallback(new WindowCallbackWrapper(window.getCallback()) {
+
+            final Drawable windowBackground = getAndRemoveWindowBackground(window.getContext());
+
+            window.setCallback(new WindowCallbackWrapper(window.getCallback()) {
                 @Override
                 public void onContentChanged() {
                     // This is called when the dialog content view is set
                     // and before the AlertController starts styling it.
+
+                    // First, we set the window background manually, if the theme specified it.
+                    if (windowBackground != null) {
+                        window.getDecorView().setBackground(windowBackground);
+                    }
 
                     // Call onContentChanged in the original callback (the dialog).
                     super.onContentChanged();
@@ -88,10 +103,10 @@ public class MaterialAlertDialog extends AlertDialog {
                 }
 
                 private void swapCustomIdsByInternalIds(Context context, View decorView) {
-                    int[] ids = new int[] {
+                    int[] ids = new int[]{
                             R.id.parentPanel, R.id.topPanel, R.id.title_template, R.id.alertTitle, R.id.contentPanel,
                             R.id.scrollView, R.id.textSpacerNoButtons, R.id.customPanel, R.id.buttonPanel};
-                    String[] idNames = new String[] {
+                    String[] idNames = new String[]{
                             "parentPanel", "topPanel", "title_template", "alertTitle", "contentPanel",
                             "scrollView", "textSpacerNoButtons", "customPanel", "buttonPanel"};
 
@@ -107,6 +122,34 @@ public class MaterialAlertDialog extends AlertDialog {
                     }
                 }
             });
+        }
+
+        /**
+         * In Lollipop, the window background drawable may have styled attributes
+         * or new drawable, which may not be support in pre-L androids.
+         * We get and remove the window background drawable from the context theme
+         * and we will later set it later manually.
+         *
+         * @param context the context from where we will get and remove the windowBackground.
+         * @return the windowBackground drawable, if defined.
+         */
+        private Drawable getAndRemoveWindowBackground(Context context) {
+            TypedArray ta = context.obtainStyledAttributes(new int[]{android.R.attr.windowBackground});
+            int windowBackgroundDrawableResId = ta.getResourceId(0, 0);
+            ta.recycle();
+
+            Drawable windowBackgroundDrawable;
+            if (windowBackgroundDrawableResId != 0) {
+                // Get the drawable.
+                MaterialResources resources = MaterialResources.getInstance(context, context.getResources());
+                windowBackgroundDrawable = resources.getDrawable(windowBackgroundDrawableResId);
+                // Remove the drawable from the theme.
+                context.getTheme().applyStyle(R.style.MaterialDialog_NoWindowBackground, true);
+            } else {
+                windowBackgroundDrawable = null;
+            }
+
+            return windowBackgroundDrawable;
         }
     }
 }
