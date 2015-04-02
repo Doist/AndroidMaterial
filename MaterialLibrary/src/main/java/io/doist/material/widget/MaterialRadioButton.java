67019d1c9a10eed5c75c921b256df1e10a646666
Rastislav Vaško <rastislav.vasko@gmail.com>
2015-04-02 23:17:03 +0200

    Adds MaterialRadioButton widget.

---
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialRadioButton.java
@@ -0,0 +1,28 @@
+package io.doist.material.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+import io.doist.material.widget.utils.MaterialWidgetHandler;
+
+public class MaterialRadioButton extends RadioButton {
+    private static final String[] sHiddenStyleables = {MaterialWidgetHandler.STYLEABLE_COMPOUND_BUTTON,
+                                                       MaterialWidgetHandler.STYLEABLE_TEXT_VIEW,
+                                                       MaterialWidgetHandler.STYLEABLE_VIEW};
+
+    public MaterialRadioButton(Context context) {
+        super(context);
+    }
+
+    public MaterialRadioButton(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.radioButtonStyle);
+    }
+
+    public MaterialRadioButton(Context context, AttributeSet attrs, int defStyle) {
+        super(MaterialWidgetHandler.themifyContext(context, attrs),
+              MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
+        MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
+        MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+}
