107ad575b73e473ac776a9f4fe9dd462d23eeeb6
Gonçalo Silva <goncalossilva@gmail.com>
2015-05-07 14:54:24 +0100

    Ensure FloatingActionButton's initial scale type is CENTER

--- MaterialLibrary/src/main/java/io/doist/material/widget/FloatingActionButton.java
+++ MaterialLibrary/src/main/java/io/doist/material/widget/FloatingActionButton.java
@@ -100,6 +100,7 @@ public class FloatingActionButton extends ImageButton {
 
         internalSetIsMini(isMini);
 
+        setScaleType(ScaleType.CENTER);
         setFocusable(true);
         setClickable(true);
     }
