58e91a20a765426d50242b0da37e779a3eb04563
Gonçalo Silva <goncalossilva@gmail.com>
2015-05-11 17:58:38 +0100

    Add MaterialRelativeLayout

---
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialRelativeLayout.java
@@ -0,0 +1,26 @@
+package io.doist.material.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import io.doist.material.widget.utils.MaterialWidgetHandler;
+
+public class MaterialRelativeLayout extends RelativeLayout {
+    private static final String[] sHiddenStyleables = {MaterialWidgetHandler.STYLEABLE_VIEW};
+
+    public MaterialRelativeLayout(Context context) {
+        this(context, null);
+    }
+
+    public MaterialRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public MaterialRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(MaterialWidgetHandler.themifyContext(context, attrs),
+              MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
+        MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
+        MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+}
