d681d98b7c939a26c00dca94f615ec24958ae563
Rastislav Vaško <rastislav.vasko@gmail.com>
2015-05-12 20:00:39 +0200

    Adds a MaterialLinearLayout.

---
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialLinearLayout.java
@@ -0,0 +1,26 @@
+package io.doist.material.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import io.doist.material.widget.utils.MaterialWidgetHandler;
+
+public class MaterialLinearLayout extends LinearLayout {
+    private static final String[] sHiddenStyleables = {MaterialWidgetHandler.STYLEABLE_VIEW};
+
+    public MaterialLinearLayout(Context context) {
+        this(context, null);
+    }
+
+    public MaterialLinearLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MaterialLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(MaterialWidgetHandler.themifyContext(context, attrs),
+              MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
+        MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
+        MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+}
