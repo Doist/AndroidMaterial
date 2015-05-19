0b343f3a27668f08d1a184e3906ba745b248a173
Rastislav Vaško <rastislav.vasko@gmail.com>
2015-05-19 22:45:08 +0200

    Adds a MaterialImageButton.

---
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialImageButton.java
@@ -0,0 +1,27 @@
+package io.doist.material.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+import io.doist.material.widget.utils.MaterialWidgetHandler;
+
+public class MaterialImageButton extends ImageButton {
+    private static final String[] sHiddenStyleables = {MaterialWidgetHandler.STYLEABLE_VIEW,
+                                                       MaterialWidgetHandler.STYLEABLE_IMAGE_VIEW};
+
+    public MaterialImageButton(Context context) {
+        this(context, null);
+    }
+
+    public MaterialImageButton(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.imageButtonStyle);
+    }
+
+    public MaterialImageButton(Context context, AttributeSet attrs, int defStyle) {
+        super(MaterialWidgetHandler.themifyContext(context, attrs),
+              MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
+        MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
+        MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+}
