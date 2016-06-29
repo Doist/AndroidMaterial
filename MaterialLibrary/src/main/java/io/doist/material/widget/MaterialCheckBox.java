f9f42fbba6f4aace0033da90f18e59f027d08f4f
Gonçalo Silva <goncalossilva@gmail.com>
2016-06-29 03:53:59 +0100

    Tint drawables after initialization
    Similar to AppCompat, override each view's methods to support tinting when set dynamically.

--- MaterialLibrary/src/main/java/io/doist/material/widget/MaterialCheckBox.java
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialCheckBox.java
@@ -1,12 +1,17 @@
 package io.doist.material.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.widget.CheckBox;
 
 import io.doist.material.widget.utils.MaterialWidgetHandler;
 
 public class MaterialCheckBox extends CheckBox {
+    private static final boolean sNative = Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT;
+
     private static final MaterialWidgetHandler.Styleable[] sHiddenStyleables = {
             MaterialWidgetHandler.Styleable.COMPOUND_BUTTON,
             MaterialWidgetHandler.Styleable.TEXT_VIEW,
@@ -26,5 +31,48 @@ public class MaterialCheckBox extends CheckBox {
               MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
         MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
         MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+
+    @Override
+    public void setButtonDrawable(@DrawableRes int resId) {
+        if (sNative) {
+            super.setButtonDrawable(resId);
+        } else {
+            super.setButtonDrawable(MaterialWidgetHandler.getDrawable(this, resId));
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(@DrawableRes int left, @DrawableRes int top,
+                                                        @DrawableRes int right, @DrawableRes int bottom) {
+        if (sNative) {
+            super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        } else {
+            super.setCompoundDrawablesWithIntrinsicBounds(
+                    MaterialWidgetHandler.getDrawable(this, left), MaterialWidgetHandler.getDrawable(this, top),
+                    MaterialWidgetHandler.getDrawable(this, right), MaterialWidgetHandler.getDrawable(this, bottom));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(@DrawableRes int start, @DrawableRes int top,
+                                                                @DrawableRes int end, @DrawableRes int bottom) {
+        if (sNative) {
+            super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        } else {
+            super.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    MaterialWidgetHandler.getDrawable(this, start), MaterialWidgetHandler.getDrawable(this, top),
+                    MaterialWidgetHandler.getDrawable(this, end), MaterialWidgetHandler.getDrawable(this, bottom));
+        }
+    }
+
+    @Override
+    public void setBackgroundResource(@DrawableRes int resId) {
+        if (sNative) {
+            super.setBackgroundResource(resId);
+        } else {
+            super.setBackground(MaterialWidgetHandler.getDrawable(this, resId));
+        }
     }
 }
--- MaterialLibrary/src/main/java/io/doist/material/widget/MaterialImageView.java
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialImageView.java
@@ -1,12 +1,16 @@
 package io.doist.material.widget;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import io.doist.material.widget.utils.MaterialWidgetHandler;
 
 public class MaterialImageView extends ImageView {
+    private static final boolean sNative = Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT;
+
     private static final MaterialWidgetHandler.Styleable[] sHiddenStyleables = {
             MaterialWidgetHandler.Styleable.VIEW,
             MaterialWidgetHandler.Styleable.IMAGE_VIEW
@@ -25,5 +29,23 @@ public class MaterialImageView extends ImageView {
               MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
         MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
         MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+
+    @Override
+    public void setBackgroundResource(@DrawableRes int resId) {
+        if (sNative) {
+            super.setBackgroundResource(resId);
+        } else {
+            super.setBackground(MaterialWidgetHandler.getDrawable(this, resId));
+        }
+    }
+
+    @Override
+    public void setImageResource(@DrawableRes int resId) {
+        if (sNative) {
+            super.setImageResource(resId);
+        } else {
+            super.setImageDrawable(MaterialWidgetHandler.getDrawable(this, resId));
+        }
     }
 }
