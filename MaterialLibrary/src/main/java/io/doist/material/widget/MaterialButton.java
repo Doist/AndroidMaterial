9d3d4b69539c4c99c89b6e8831fca1684beb8702
Rastislav Vaško <rastislav.vasko@gmail.com>
2016-10-06 15:56:27 +0200

    Use Material drawables for compound drawables in MaterialButton

--- MaterialLibrary/src/main/java/io/doist/material/widget/MaterialButton.java
+++ MaterialLibrary/src/main/java/io/doist/material/widget/MaterialButton.java
@@ -1,5 +1,6 @@
 package io.doist.material.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.DrawableRes;
@@ -12,6 +13,7 @@ public class MaterialButton extends Button {
     private static final boolean sNative = Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT;
 
     private static final MaterialWidgetHandler.Styleable[] sHiddenStyleables = {
+            MaterialWidgetHandler.Styleable.TEXT_VIEW,
             MaterialWidgetHandler.Styleable.VIEW
     };
 
@@ -28,6 +30,31 @@ public class MaterialButton extends Button {
               MaterialWidgetHandler.hideStyleableAttributes(attrs, sHiddenStyleables), defStyle);
         MaterialWidgetHandler.restoreStyleableAttributes(sHiddenStyleables);
         MaterialWidgetHandler.init(this, attrs, defStyle, sHiddenStyleables);
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(@DrawableRes int left, @DrawableRes int top,
+                                                        @DrawableRes int right, @DrawableRes int bottom) {
+        if (sNative) {
+            super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        } else {
+            super.setCompoundDrawablesWithIntrinsicBounds(
+                    MaterialWidgetHandler.getDrawable(this, left), MaterialWidgetHandler.getDrawable(this, top),
+                    MaterialWidgetHandler.getDrawable(this, right), MaterialWidgetHandler.getDrawable(this, bottom));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void setCompoundDrawablesRelativeWithIntrinsicBounds(@DrawableRes int start, @DrawableRes int top,
+                                                                @DrawableRes int end, @DrawableRes int bottom) {
+        if (sNative) {
+            super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        } else {
+            super.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    MaterialWidgetHandler.getDrawable(this, start), MaterialWidgetHandler.getDrawable(this, top),
+                    MaterialWidgetHandler.getDrawable(this, end), MaterialWidgetHandler.getDrawable(this, bottom));
+        }
     }
 
     @Override
